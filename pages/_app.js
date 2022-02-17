6f85420838d5513c4dd511647eaefa0f24f7ee9b
Michael Liendo <mtliendo@gmail.com>
2022-02-17 09:55:24 -0600

    contact-form-starter

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-
+import '@aws-amplify/ui-react/styles.css'
+import { AmplifyProvider } from '@aws-amplify/ui-react'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<AmplifyProvider>
+			<Component {...pageProps} />
+		</AmplifyProvider>
+	)
 }
 
 export default MyApp
